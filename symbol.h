15031d549a453a41259d9ec30dcf1fd2f481ece8
WesternGreen <westerngreen@126.com>
2018-11-12 18:19:47 +0800

    fix and update symbol.h - discard symbol.cpp and move template functions to symbol.h - fix operator==() - add operator!=()

--- symbol.h
+++ symbol.h
@@ -15,7 +15,8 @@ struct Symbol
 	SymbolType type;
 	int index; // index in SymbolTable
 
-	bool operator=(const Symbol &ano) { return ano.type == type && ano.index == index; }
+	bool operator==(const Symbol &ano) const { return ano.type == type && ano.index == index; }
+	bool operator!=(const Symbol &ano) const { return !(*this == ano); }
 };
 
 template <bool isTerminatorTable>
@@ -28,10 +29,41 @@ private:
 public:
 	SymbolTable();
 	int getIndex(const string &str); // if str not exist, push it into symbols
-	string getStr(int i)const;						 // return blank string if i is invalid
+	string getStr(int i) const;			 // return blank string if i is invalid
 };
 
 const Symbol EPSILON = {Symbol::SymbolType::T, 0};
 
 typedef SymbolTable<true> T_Table;
 typedef SymbolTable<false> NT_Table;
+
+template <>
+inline SymbolTable<true>::SymbolTable()
+{
+	getIndex("~"); // insert EPSILON to terminatorTable
+}
+
+template <bool b>
+SymbolTable<b>::SymbolTable() {}
+
+template <bool b>
+int SymbolTable<b>::getIndex(const string &str)
+{
+	for (int i = 0; i < symbols.size(); ++i)
+	{
+		if (symbols[i] == str) // str exist
+			return i;
+	}
+	// str does NOT exist, add it to table
+	symbols.push_back(str);
+	return symbols.size() - 1;
+}
+
+template <bool b>
+string SymbolTable<b>::getStr(int i) const
+{
+	if (i >= 0 && i < symbols.size())
+		return symbols[i];
+	else
+		return "";
+}
